116e9cabd3df7c71c233b5db24980cac157ab004
Ionut <ionutputinicabz@gmail.com>
2020-10-09 14:10:11 +0200

    Fixed bug where 2 installs of the app are created

--- unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++ unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
@@ -1,2 +1,2 @@
 #include "il2cpp-config.h"
-#include "D:\Projects\FlyIt_Android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
+#include "D:\FlyIt-Android\FlyIt-Android\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
